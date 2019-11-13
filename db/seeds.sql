5853b817e0e3033ea7eb393762fc890011271d1d
Anthony Yonta <atischillin@gmail.com>
2019-11-12 23:35:02 -0500

    uploading framwork and entire site, had to do most of this project offline

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Obi');
+INSERT INTO burgers (name) VALUES ('Yoda');
+INSERT INTO burgers (name, devoured) VALUES ('Ewok', true);
+INSERT INTO burgers (name, devoured) VALUES ('Wookie', true);
+INSERT INTO burgers (name, devoured) VALUES ('Lightsaber', true);
+INSERT INTO burgers (name) VALUES ('Darth');
